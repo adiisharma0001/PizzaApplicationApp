a492c14bf2e177e5924571abe5f14a73ff5f2b26
Aditya <95631458+Sharmaaditya05@users.noreply.github.com>
2023-05-03 22:10:35 +0530

    .

--- src/components/card.js
+++ src/components/card.js
@@ -12,8 +12,8 @@ export default function card(props) {
                 <div className="card mt-3 mx-3" style={{ "width": "18rem", "maxHeight": "420px" }}  >
                     <img src={props.img} className="card-img-top" alt="..." style={{ height: "200px", objectFit: "fill" }} />
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is Some Important Text.</p>
+                        <h5 className="card-title">Card Title</h5>
+                        <p className="card-text">This is Some Important Text</p>
                         <div className='container w-100'>
                             <select className='m-2 h-100  bg-success rounded'>
                                 {Array.from(Array(10), (e, i) => {
